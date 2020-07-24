b6edb78054b3f88ecf2f4bb4cffec332aba3d93f
TuzDmitry <dimatuz94@gmail.com>
2020-07-24 10:43:13 +0300

    Updates

---
+++ precache-manifest.e80ad56a3f504b4b6738f530d22b4ddc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1428f6ed0a018740245c1ebbce798960",
+    "url": "/joint_cards_project/index.html"
+  },
+  {
+    "revision": "9fb9f2ec478e9891ac5c",
+    "url": "/joint_cards_project/static/css/main.9247fc1f.chunk.css"
+  },
+  {
+    "revision": "c9ab6f5917d06a9d1277",
+    "url": "/joint_cards_project/static/js/2.eb46c871.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/joint_cards_project/static/js/2.eb46c871.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9fb9f2ec478e9891ac5c",
+    "url": "/joint_cards_project/static/js/main.c1386d30.chunk.js"
+  },
+  {
+    "revision": "d2363c523c41a2913c67",
+    "url": "/joint_cards_project/static/js/runtime-main.2a94473e.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/TuzDmitry.github.io/joint_cards_project/precache-manifest.fe0d5cea96c4383d4536f8033cce7f85.js"
+  "/joint_cards_project/precache-manifest.e80ad56a3f504b4b6738f530d22b4ddc.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/TuzDmitry.github.io/joint_cards_project/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/joint_cards_project/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
